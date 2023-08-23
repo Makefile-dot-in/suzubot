5256e817d1345defb8b5cd80c1fa5da4d3349470
Author <me@example.com>
2023-08-23 20:10:08 +0300

    forgot to add two files

---
+++ src/errors/global_handler.rs
@@ -0,0 +1,83 @@
+use poise::{FrameworkError, ApplicationCommandOrAutocompleteInteraction};
+use poise::serenity_prelude as ser;
+
+use crate::errors::LogError;
+
+use super::Error;
+use super::{LoggedFrameworkError, LoggedMappedWithContext, LoggedError};
+
+fn suzu_error_level(err: &Error) -> log::Level {
+	match err {
+		Error::Internal(_) => log::Level::Warn,
+		_ => log::Level::Info,
+	}
+}
+
+fn framework_error_level(err: &FrameworkError<'_, crate::Data, crate::SuzuError>) -> log::Level {
+	use FrameworkError::*;
+	match err {
+		Setup { .. } => log::Level::Error,
+		EventHandler { error, .. } => suzu_error_level(&error.error),
+		Command { error, .. } => suzu_error_level(&error.error),
+		CommandPanic { .. } => log::Level::Error,
+		ArgumentParse { .. } => log::Level::Info,
+		CommandStructureMismatch { .. } => log::Level::Warn,
+		CooldownHit { .. } => log::Level::Info,
+		MissingBotPermissions { .. } => log::Level::Info,
+		MissingUserPermissions { .. } => log::Level::Info,
+		NotAnOwner { .. } => log::Level::Info,
+		GuildOnly { .. } => log::Level::Info,
+		DmOnly { .. } => log::Level::Info,
+		NsfwOnly { .. } => log::Level::Info,
+		CommandCheckFailed { error: Some(error), .. } => suzu_error_level(&error.error),
+		CommandCheckFailed { error: None, .. } => log::Level::Info,
+		DynamicPrefix { error, .. } => suzu_error_level(&error.error),
+		UnknownCommand { .. } => log::Level::Warn,
+		UnknownInteraction { .. } => log::Level::Warn,
+		_ => log::Level::Warn,
+	}
+}
+
+async fn discord_report_message(err: &FrameworkError<'_, crate::Data, crate::SuzuError>) {
+	use FrameworkError::*;
+	match err {
+		CommandPanic { ctx, .. } => {
+			ctx.say("Command crashed.").await.logerr();
+		},
+		CommandStructureMismatch { ctx, .. } => {
+			ctx.say("Command failed due to a bot configuration error.").await.logerr();
+		},
+		CooldownHit { remaining_cooldown, ctx, .. } => {
+			ctx.say(format!("Cooldown not yet expired ({seconds} seconds remaining)",
+							seconds = remaining_cooldown.as_secs())).await.logerr();
+		},
+		DmOnly { ctx, .. } => {
+			ctx.say("This command may only be used in DMs.").await.logerr();
+		},
+		NsfwOnly { ctx, .. } => {
+			ctx.say("This command may only be used in NSFW channels.").await.logerr();
+		},
+		MissingBotPermissions { missing_permissions, ctx } => {
+			ctx.say(format!("The bot is missing permissions {missing_permissions}")).await.logerr();
+		}
+		CommandCheckFailed { error: Some(error), ctx } => {
+			ctx.say(format!("{error}")).await.logerr();
+		},
+		UnknownInteraction { ctx, interaction: ApplicationCommandOrAutocompleteInteraction::ApplicationCommand(interaction), .. } => {
+			interaction.create_interaction_response(ctx, |r| {
+				r.kind(ser::InteractionResponseType::ChannelMessageWithSource)
+					.interaction_response_data(|d| d.content("Unknown interaction"))
+			}).await.logerr();
+		},
+		UnknownInteraction { interaction: ApplicationCommandOrAutocompleteInteraction::Autocomplete(_), .. } => {},
+		_ => {}
+	};
+}
+
+pub async fn on_error(err: FrameworkError<'_, crate::Data, crate::SuzuError>) {
+	let log_level = framework_error_level(&err);
+	log::log!(log_level, "{logged_err}",
+			  logged_err = LoggedFrameworkError(&err));
+
+	discord_report_message(&err).await;
+}
---
+++ src/init.rs
@@ -0,0 +1,93 @@
+use anyhow::{Context, anyhow};
+use poise::serenity_prelude as ser;
+use crate::errors::global_handler::on_error;
+use crate::log::LogData;
+use crate::webhook::WebhookExecutor;
+use std::collections::{HashSet, HashMap};
+use std::env;
+use std::error::Error as StdError;
+use std::str::FromStr;
+use serde::{Deserialize, Serialize};
+use crate::{CustomCommandData, DEF_CMD_DATA, PoiseContext};
+
+#[poise::command(prefix_command, owners_only, custom_data = "CustomCommandData { test_mode: true, ..DEF_CMD_DATA }")]
+pub async fn register(ctx: PoiseContext<'_>) -> crate::errors::Result<()> {
+	Ok(poise::builtins::register_application_commands_buttons(ctx).await?)
+}
+
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct Profile {
+	owner_ids: HashSet<ser::UserId>,
+	#[serde(default = "Profile::default_bot_name")]
+	bot_name: String,
+	token: String,
+	#[serde(default)]
+	test_mode: bool,
+	dbconnstr: String,
+}
+
+impl Profile {
+	fn default_bot_name() -> String {
+		"Suzu".to_owned()
+	}
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct Config {
+	#[serde(default = "Config::default_default_profile")]
+	default: String,
+	#[serde(flatten)]
+	profiles: HashMap<String, Profile>,
+}
+
+impl Config {
+	fn default_default_profile() -> String {
+		"default".to_owned()
+	}
+
+	pub fn get_profile(mut self, name: Option<&str>) -> Result<Profile, anyhow::Error> {
+		let name = name.unwrap_or(&self.default);
+		self.profiles.remove(name)
+			.ok_or_else(|| anyhow!("{name}: profile not found"))
+	}
+}
+
+pub async fn run(profile: Profile) -> Result<(), anyhow::Error> {
+	let framework = poise::Framework::builder()
+		.options(poise::FrameworkOptions {
+			commands: super::COMMANDS.iter()
+				.map(|f| f())
+				.filter(|cmd| CustomCommandData::from_command_data(cmd).test_mode)
+				.collect::<Vec<_>>(),
+			owners: profile.owner_ids,
+			on_error: |err| Box::pin(on_error(err)),
+			..Default::default()
+		})
+		.token(profile.token)
+		.intents(ser::GatewayIntents::all())
+		.setup(move |ctx, _ready, framework| {
+			Box::pin(async move {
+				if !profile.test_mode {
+					poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+				}
+				Ok(crate::Data {
+					bot_name: profile.bot_name,
+					webhexec: WebhookExecutor::new(),
+					dbconn: {
+						let manager = bb8_postgres::PostgresConnectionManager
+							::new_from_stringlike(profile.dbconnstr, crate::pg::NoTls)?;
+						bb8::Pool::builder().build(manager).await?
+					},
+					logdata: LogData::new(),
+				})
+			})
+		})
+		.build()
+		.await?;
+
+	framework.start().await?;
+	Ok(())
+}
